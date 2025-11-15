340da5760868a23f3bedd7d49112bedaac213f4e
Nguyễn Hữu Giáp <giap13022005@@gmail.com>
2025-11-15 16:49:19 +0700

    update sidebar add hoa don ban

--- BagShopManagement/Views/Common/QuanLiBanHang.cs
+++ BagShopManagement/Views/Common/QuanLiBanHang.cs
@@ -147,7 +147,7 @@ namespace BagShopManagement.Views.Common
 
             // Dev2 Events (Đã có)
             sideBarControl.SanPhamClicked += (s, ev) => ShowUserControl<SanPhamControl>();
-            //sideBarControl.ShowQuanLyHoaDonClicked += (s, ev) => ShowUserControl<U>();
+            sideBarControl.ShowQuanLyHoaDonClicked += (s, ev) => ShowUserControl<UC_HoaDonBan>();
 
             // Dev3 Events (Đã có)
             sideBarControl.NhaCungCapClicked += (s, ev) => ShowUserControl<NhaCungCapControl>();
