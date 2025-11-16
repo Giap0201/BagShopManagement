53e660e693e66a3bf11f10a5dea135170d9743f5
ThanhF-Xuaan <le227460@gmail.com>
2025-11-16 23:03:03 +0700

    header & sidebar

--- BagShopManagement/Views/Controls/HeaderControl.Designer.cs
+++ BagShopManagement/Views/Controls/HeaderControl.Designer.cs
@@ -29,16 +29,30 @@
         private void InitializeComponent()
         {
             panelHeader = new Panel();
+            label1 = new Label();
+            panelHeader.SuspendLayout();
             SuspendLayout();
             // 
             // panelHeader
             // 
-            panelHeader.BackColor = SystemColors.MenuHighlight;
+            panelHeader.BackColor = Color.FromArgb(64, 78, 103);
+            panelHeader.Controls.Add(label1);
             panelHeader.Dock = DockStyle.Top;
             panelHeader.Location = new Point(0, 0);
             panelHeader.Name = "panelHeader";
             panelHeader.Size = new Size(605, 80);
             panelHeader.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(0, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(297, 38);
+            label1.TabIndex = 0;
+            label1.Text = "QUẢN LÝ BÁN HÀNG";
             // 
             // HeaderControl
             // 
@@ -48,11 +62,14 @@
             Controls.Add(panelHeader);
             Name = "HeaderControl";
             Size = new Size(605, 80);
+            panelHeader.ResumeLayout(false);
+            panelHeader.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panelHeader;
+        private Label label1;
     }
 }
