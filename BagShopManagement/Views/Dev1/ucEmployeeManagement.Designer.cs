8eb299d43b2b2207f3535f5986280804372db8f7
ThanhF-Xuaan <le227460@gmail.com>
2025-11-17 00:23:27 +0700

    fix view

--- BagShopManagement/Views/Dev1/ucEmployeeManagement.Designer.cs
+++ BagShopManagement/Views/Dev1/ucEmployeeManagement.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            lblTitle = new Label();
             pnlToolbar = new Panel();
             txtTimKiem = new TextBox();
             btnTimKiem = new Button();
@@ -41,20 +40,9 @@
             ((System.ComponentModel.ISupportInitialize)dgvNhanVien).BeginInit();
             SuspendLayout();
             // 
-            // lblTitle
-            // 
-            lblTitle.AutoSize = true;
-            lblTitle.Dock = DockStyle.Top;
-            lblTitle.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblTitle.Location = new Point(0, 0);
-            lblTitle.Name = "lblTitle";
-            lblTitle.Size = new Size(247, 31);
-            lblTitle.TabIndex = 0;
-            lblTitle.Text = "QUẢN LÝ NHÂN VIÊN";
-            lblTitle.TextAlign = ContentAlignment.MiddleCenter;
-            // 
             // pnlToolbar
             // 
+            pnlToolbar.BackColor = SystemColors.ButtonFace;
             pnlToolbar.Controls.Add(txtTimKiem);
             pnlToolbar.Controls.Add(btnTimKiem);
             pnlToolbar.Controls.Add(btnKhoaMo);
@@ -62,65 +50,91 @@
             pnlToolbar.Controls.Add(btnThem);
             pnlToolbar.Controls.Add(btnLamMoi);
             pnlToolbar.Dock = DockStyle.Top;
-            pnlToolbar.Location = new Point(0, 31);
+            pnlToolbar.Location = new Point(0, 0);
             pnlToolbar.Name = "pnlToolbar";
-            pnlToolbar.Size = new Size(985, 111);
+            pnlToolbar.Size = new Size(985, 166);
             pnlToolbar.TabIndex = 1;
             // 
             // txtTimKiem
             // 
-            txtTimKiem.Location = new Point(176, 79);
+            txtTimKiem.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtTimKiem.Location = new Point(172, 102);
             txtTimKiem.Name = "txtTimKiem";
-            txtTimKiem.Size = new Size(249, 27);
+            txtTimKiem.Size = new Size(249, 30);
             txtTimKiem.TabIndex = 5;
             // 
             // btnTimKiem
             // 
-            btnTimKiem.Location = new Point(26, 79);
+            btnTimKiem.BackColor = SystemColors.Highlight;
+            btnTimKiem.FlatAppearance.BorderSize = 0;
+            btnTimKiem.FlatStyle = FlatStyle.Flat;
+            btnTimKiem.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            btnTimKiem.ForeColor = Color.White;
+            btnTimKiem.Location = new Point(26, 95);
             btnTimKiem.Name = "btnTimKiem";
-            btnTimKiem.Size = new Size(94, 29);
+            btnTimKiem.Size = new Size(94, 37);
             btnTimKiem.TabIndex = 4;
             btnTimKiem.Text = "Tìm kiếm";
-            btnTimKiem.UseVisualStyleBackColor = true;
+            btnTimKiem.UseVisualStyleBackColor = false;
             // 
             // btnKhoaMo
             // 
+            btnKhoaMo.BackColor = Color.FromArgb(231, 76, 60);
+            btnKhoaMo.FlatAppearance.BorderSize = 0;
+            btnKhoaMo.FlatStyle = FlatStyle.Flat;
+            btnKhoaMo.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            btnKhoaMo.ForeColor = Color.White;
             btnKhoaMo.Location = new Point(579, 27);
             btnKhoaMo.Name = "btnKhoaMo";
-            btnKhoaMo.Size = new Size(138, 29);
+            btnKhoaMo.Size = new Size(138, 39);
             btnKhoaMo.TabIndex = 3;
             btnKhoaMo.Text = "Khóa / Mở khóa";
-            btnKhoaMo.UseVisualStyleBackColor = true;
+            btnKhoaMo.UseVisualStyleBackColor = false;
             btnKhoaMo.Click += btnKhoaMo_Click;
             // 
             // btnSua
             // 
+            btnSua.BackColor = Color.FromArgb(224, 224, 224);
+            btnSua.FlatAppearance.BorderSize = 0;
+            btnSua.FlatStyle = FlatStyle.Flat;
+            btnSua.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            btnSua.ForeColor = Color.FromArgb(51, 51, 51);
             btnSua.Location = new Point(389, 27);
             btnSua.Name = "btnSua";
-            btnSua.Size = new Size(94, 29);
+            btnSua.Size = new Size(94, 39);
             btnSua.TabIndex = 2;
             btnSua.Text = "Sửa";
-            btnSua.UseVisualStyleBackColor = true;
+            btnSua.UseVisualStyleBackColor = false;
             btnSua.Click += btnSua_Click;
             // 
             // btnThem
             // 
+            btnThem.BackColor = SystemColors.Highlight;
+            btnThem.FlatAppearance.BorderSize = 0;
+            btnThem.FlatStyle = FlatStyle.Flat;
+            btnThem.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            btnThem.ForeColor = Color.White;
             btnThem.Location = new Point(209, 27);
             btnThem.Name = "btnThem";
-            btnThem.Size = new Size(94, 29);
+            btnThem.Size = new Size(120, 39);
             btnThem.TabIndex = 1;
             btnThem.Text = "Thêm mới";
-            btnThem.UseVisualStyleBackColor = true;
+            btnThem.UseVisualStyleBackColor = false;
             btnThem.Click += btnThem_Click;
             // 
             // btnLamMoi
             // 
+            btnLamMoi.BackColor = Color.White;
+            btnLamMoi.FlatAppearance.BorderSize = 0;
+            btnLamMoi.FlatStyle = FlatStyle.Flat;
+            btnLamMoi.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            btnLamMoi.ForeColor = Color.FromArgb(64, 78, 103);
             btnLamMoi.Location = new Point(26, 27);
             btnLamMoi.Name = "btnLamMoi";
-            btnLamMoi.Size = new Size(94, 29);
+            btnLamMoi.Size = new Size(116, 39);
             btnLamMoi.TabIndex = 0;
             btnLamMoi.Text = "Làm mới (F5)";
-            btnLamMoi.UseVisualStyleBackColor = true;
+            btnLamMoi.UseVisualStyleBackColor = false;
             btnLamMoi.Click += btnLamMoi_Click;
             // 
             // dgvNhanVien
@@ -130,13 +144,13 @@
             dgvNhanVien.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvNhanVien.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvNhanVien.Dock = DockStyle.Fill;
-            dgvNhanVien.Location = new Point(0, 142);
+            dgvNhanVien.Location = new Point(0, 166);
             dgvNhanVien.MultiSelect = false;
             dgvNhanVien.Name = "dgvNhanVien";
             dgvNhanVien.ReadOnly = true;
             dgvNhanVien.RowHeadersWidth = 51;
             dgvNhanVien.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvNhanVien.Size = new Size(985, 478);
+            dgvNhanVien.Size = new Size(985, 454);
             dgvNhanVien.TabIndex = 2;
             dgvNhanVien.CellFormatting += dgvNhanVien_CellFormatting;
             // 
@@ -146,7 +160,6 @@
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(dgvNhanVien);
             Controls.Add(pnlToolbar);
-            Controls.Add(lblTitle);
             Name = "ucEmployeeManagement";
             Size = new Size(985, 620);
             Load += ucEmployeeManagement_Load;
@@ -154,12 +167,9 @@
             pnlToolbar.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dgvNhanVien).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label lblTitle;
         private Panel pnlToolbar;
         private TextBox txtTimKiem;
         private Button btnTimKiem;
