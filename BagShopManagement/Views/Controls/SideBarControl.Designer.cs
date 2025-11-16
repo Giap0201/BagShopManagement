c7d3c5bf14fb54a01fb13c54387c8d349a752631
ThanhF-Xuaan <le227460@gmail.com>
2025-11-16 23:03:03 +0700

    header & sidebar

--- BagShopManagement/Views/Controls/SideBarControl.Designer.cs
+++ BagShopManagement/Views/Controls/SideBarControl.Designer.cs
@@ -51,7 +51,7 @@
             // 
             // panel1
             // 
-            panel1.BackColor = SystemColors.Info;
+            panel1.BackColor = Color.FromArgb(64, 78, 103);
             panel1.Controls.Add(btnBCTK);
             panel1.Controls.Add(btnKhuyenMai);
             panel1.Controls.Add(btnTonKho);
@@ -73,128 +73,199 @@
             // 
             // btnBCTK
             // 
-            btnBCTK.Location = new Point(0, 620);
+            btnBCTK.BackColor = Color.FromArgb(176, 199, 222);
+            btnBCTK.FlatAppearance.BorderSize = 0;
+            btnBCTK.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnBCTK.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnBCTK.FlatStyle = FlatStyle.Flat;
+            btnBCTK.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBCTK.Location = new Point(0, 673);
             btnBCTK.Name = "btnBCTK";
-            btnBCTK.Size = new Size(330, 57);
+            btnBCTK.Size = new Size(330, 58);
             btnBCTK.TabIndex = 12;
             btnBCTK.Text = "Báo cáo thống kê";
-            btnBCTK.UseVisualStyleBackColor = true;
+            btnBCTK.UseVisualStyleBackColor = false;
             btnBCTK.Click += btnBCTK_Click;
             // 
             // btnKhuyenMai
             // 
-            btnKhuyenMai.Location = new Point(0, 676);
+            btnKhuyenMai.BackColor = Color.FromArgb(176, 199, 222);
+            btnKhuyenMai.FlatAppearance.BorderSize = 0;
+            btnKhuyenMai.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnKhuyenMai.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnKhuyenMai.FlatStyle = FlatStyle.Flat;
+            btnKhuyenMai.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnKhuyenMai.Location = new Point(0, 619);
             btnKhuyenMai.Name = "btnKhuyenMai";
-            btnKhuyenMai.Size = new Size(330, 57);
+            btnKhuyenMai.Size = new Size(330, 58);
             btnKhuyenMai.TabIndex = 11;
-            btnKhuyenMai.Text = "Khuyến Mãi";
-            btnKhuyenMai.UseVisualStyleBackColor = true;
+            btnKhuyenMai.Text = "Khuyến mãi";
+            btnKhuyenMai.UseVisualStyleBackColor = false;
             btnKhuyenMai.Click += btnKhuyenMai_Click;
             // 
             // btnTonKho
             // 
-            btnTonKho.BackColor = SystemColors.Info;
-            btnTonKho.Location = new Point(-3, 508);
+            btnTonKho.BackColor = Color.FromArgb(176, 199, 222);
+            btnTonKho.FlatAppearance.BorderSize = 0;
+            btnTonKho.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnTonKho.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnTonKho.FlatStyle = FlatStyle.Flat;
+            btnTonKho.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnTonKho.Location = new Point(0, 566);
             btnTonKho.Name = "btnTonKho";
-            btnTonKho.Size = new Size(330, 57);
+            btnTonKho.Size = new Size(330, 58);
             btnTonKho.TabIndex = 9;
-            btnTonKho.Text = "Tồn Kho";
+            btnTonKho.Text = "Tồn kho";
             btnTonKho.UseVisualStyleBackColor = false;
             btnTonKho.Click += btnTonKho_Click;
             // 
             // btnHoaDonNhap
             // 
-            btnHoaDonNhap.Location = new Point(-3, 565);
+            btnHoaDonNhap.BackColor = Color.FromArgb(176, 199, 222);
+            btnHoaDonNhap.FlatAppearance.BorderSize = 0;
+            btnHoaDonNhap.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnHoaDonNhap.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnHoaDonNhap.FlatStyle = FlatStyle.Flat;
+            btnHoaDonNhap.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnHoaDonNhap.Location = new Point(0, 511);
             btnHoaDonNhap.Name = "btnHoaDonNhap";
-            btnHoaDonNhap.Size = new Size(330, 57);
+            btnHoaDonNhap.Size = new Size(330, 58);
             btnHoaDonNhap.TabIndex = 10;
             btnHoaDonNhap.Text = "Hoá đơn nhập";
-            btnHoaDonNhap.UseVisualStyleBackColor = true;
+            btnHoaDonNhap.UseVisualStyleBackColor = false;
             btnHoaDonNhap.Click += btnHoaDonNhap_Click;
             // 
             // btnTaiKhoan
             // 
-            btnTaiKhoan.Location = new Point(-3, 456);
+            btnTaiKhoan.BackColor = Color.FromArgb(176, 199, 222);
+            btnTaiKhoan.FlatAppearance.BorderSize = 0;
+            btnTaiKhoan.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnTaiKhoan.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnTaiKhoan.FlatStyle = FlatStyle.Flat;
+            btnTaiKhoan.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnTaiKhoan.Location = new Point(0, 456);
             btnTaiKhoan.Name = "btnTaiKhoan";
-            btnTaiKhoan.Size = new Size(330, 57);
+            btnTaiKhoan.Size = new Size(330, 58);
             btnTaiKhoan.TabIndex = 8;
             btnTaiKhoan.Text = "Tài khoản";
-            btnTaiKhoan.UseVisualStyleBackColor = true;
+            btnTaiKhoan.UseVisualStyleBackColor = false;
             btnTaiKhoan.Click += btnTaiKhoan_Click;
             // 
             // btnNCC
             // 
-            btnNCC.Location = new Point(-2, 408);
+            btnNCC.BackColor = Color.FromArgb(176, 199, 222);
+            btnNCC.FlatAppearance.BorderSize = 0;
+            btnNCC.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnNCC.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnNCC.FlatStyle = FlatStyle.Flat;
+            btnNCC.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNCC.Location = new Point(0, 401);
             btnNCC.Name = "btnNCC";
-            btnNCC.Size = new Size(330, 57);
+            btnNCC.Size = new Size(330, 58);
             btnNCC.TabIndex = 7;
             btnNCC.Text = "Nhà cung cấp";
-            btnNCC.UseVisualStyleBackColor = true;
+            btnNCC.UseVisualStyleBackColor = false;
             btnNCC.Click += btnNCC_Click;
             // 
             // btnNhanVien
             // 
-            btnNhanVien.Location = new Point(0, 357);
+            btnNhanVien.BackColor = Color.FromArgb(176, 199, 222);
+            btnNhanVien.FlatAppearance.BorderSize = 0;
+            btnNhanVien.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnNhanVien.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnNhanVien.FlatStyle = FlatStyle.Flat;
+            btnNhanVien.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNhanVien.Location = new Point(0, 347);
             btnNhanVien.Name = "btnNhanVien";
-            btnNhanVien.Size = new Size(330, 57);
+            btnNhanVien.Size = new Size(330, 58);
             btnNhanVien.TabIndex = 6;
             btnNhanVien.Text = "Nhân viên";
-            btnNhanVien.UseVisualStyleBackColor = true;
+            btnNhanVien.UseVisualStyleBackColor = false;
             btnNhanVien.Click += btnNhanVien_Click;
             // 
             // btnKhachHang
             // 
-            btnKhachHang.Location = new Point(-2, 303);
+            btnKhachHang.BackColor = Color.FromArgb(176, 199, 222);
+            btnKhachHang.FlatAppearance.BorderSize = 0;
+            btnKhachHang.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnKhachHang.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnKhachHang.FlatStyle = FlatStyle.Flat;
+            btnKhachHang.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnKhachHang.Location = new Point(0, 294);
             btnKhachHang.Name = "btnKhachHang";
-            btnKhachHang.Size = new Size(330, 57);
+            btnKhachHang.Size = new Size(330, 58);
             btnKhachHang.TabIndex = 5;
             btnKhachHang.Text = "Khách hàng";
-            btnKhachHang.UseVisualStyleBackColor = true;
+            btnKhachHang.UseVisualStyleBackColor = false;
             btnKhachHang.Click += btnKhachHang_Click;
             // 
             // btnHoaDonBan
             // 
-            btnHoaDonBan.Location = new Point(-3, 250);
+            btnHoaDonBan.BackColor = Color.FromArgb(176, 199, 222);
+            btnHoaDonBan.FlatAppearance.BorderSize = 0;
+            btnHoaDonBan.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnHoaDonBan.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnHoaDonBan.FlatStyle = FlatStyle.Flat;
+            btnHoaDonBan.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnHoaDonBan.Location = new Point(0, 239);
             btnHoaDonBan.Name = "btnHoaDonBan";
-            btnHoaDonBan.Size = new Size(330, 57);
+            btnHoaDonBan.Size = new Size(330, 58);
             btnHoaDonBan.TabIndex = 4;
             btnHoaDonBan.Text = "Hoá đơn bán";
-            btnHoaDonBan.UseVisualStyleBackColor = true;
+            btnHoaDonBan.UseVisualStyleBackColor = false;
             btnHoaDonBan.Click += btnHoaDonBan_Click;
             // 
             // btnSanPham
             // 
-            btnSanPham.Location = new Point(0, 199);
+            btnSanPham.BackColor = Color.FromArgb(176, 199, 222);
+            btnSanPham.FlatAppearance.BorderSize = 0;
+            btnSanPham.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnSanPham.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnSanPham.FlatStyle = FlatStyle.Flat;
+            btnSanPham.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSanPham.Location = new Point(0, 185);
             btnSanPham.Name = "btnSanPham";
-            btnSanPham.Size = new Size(330, 57);
+            btnSanPham.Size = new Size(330, 58);
             btnSanPham.TabIndex = 3;
             btnSanPham.Text = "Sản phẩm";
-            btnSanPham.UseVisualStyleBackColor = true;
+            btnSanPham.UseVisualStyleBackColor = false;
             btnSanPham.Click += btnSanPham_Click;
             // 
             // btnDanhMuc
             // 
-            btnDanhMuc.Location = new Point(0, 143);
+            btnDanhMuc.BackColor = Color.FromArgb(176, 199, 222);
+            btnDanhMuc.FlatAppearance.BorderSize = 0;
+            btnDanhMuc.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnDanhMuc.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnDanhMuc.FlatStyle = FlatStyle.Flat;
+            btnDanhMuc.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDanhMuc.Location = new Point(0, 130);
             btnDanhMuc.Name = "btnDanhMuc";
-            btnDanhMuc.Size = new Size(330, 57);
+            btnDanhMuc.Size = new Size(330, 58);
             btnDanhMuc.TabIndex = 2;
             btnDanhMuc.Text = "Danh mục ";
-            btnDanhMuc.UseVisualStyleBackColor = true;
+            btnDanhMuc.UseVisualStyleBackColor = false;
             btnDanhMuc.Click += btnDanhMuc_Click;
             // 
             // btnBanHang
             // 
-            btnBanHang.Location = new Point(0, 86);
+            btnBanHang.BackColor = Color.FromArgb(176, 199, 222);
+            btnBanHang.FlatAppearance.BorderSize = 0;
+            btnBanHang.FlatAppearance.MouseDownBackColor = SystemColors.Highlight;
+            btnBanHang.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 90, 115);
+            btnBanHang.FlatStyle = FlatStyle.Flat;
+            btnBanHang.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBanHang.Location = new Point(0, 75);
             btnBanHang.Name = "btnBanHang";
-            btnBanHang.Size = new Size(330, 57);
+            btnBanHang.Size = new Size(330, 58);
             btnBanHang.TabIndex = 1;
             btnBanHang.Text = "Bán hàng";
-            btnBanHang.UseVisualStyleBackColor = true;
+            btnBanHang.UseVisualStyleBackColor = false;
             btnBanHang.Click += btnBanHang_Click;
             // 
             // button1
             // 
-            button1.BackColor = SystemColors.Info;
+            button1.BackColor = Color.FromArgb(176, 199, 222);
             button1.Dock = DockStyle.Top;
             button1.Font = new Font("Segoe Script", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             button1.ForeColor = SystemColors.ActiveCaptionText;
