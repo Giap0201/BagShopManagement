440347e37f68aa0ec92ec2cd217f2e0a493e4efc
ngkhson <nkhanhson25112005@gmail.com>
2025-11-19 22:25:10 +0700

    push

--- BagShopManagement/Properties/Resources.Designer.cs
+++ BagShopManagement/Properties/Resources.Designer.cs
@@ -243,6 +243,16 @@ namespace BagShopManagement.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap tui {
+            get {
+                object obj = ResourceManager.GetObject("tui", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap user {
             get {
                 object obj = ResourceManager.GetObject("user", resourceCulture);
